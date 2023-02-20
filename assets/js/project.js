f879be93b73da8781fe20610938d782e812e04d2
SouabeRaphael <R.souabe03@gmail.com>
2023-02-20 17:25:03 +0100

    finalisation

--- assets/js/project.js
+++ assets/js/project.js
@@ -96,47 +96,45 @@ fetch('data/project.json')
 // ---------------------------------------------------------------
 
 window.addEventListener("scroll", function() {
-    setTimeout(function(){
-        let network_and_sound = document.querySelector('.project_page_bottom');
-        let header_logo_back = document.querySelector('.header_logo_back');
-        let header_logo_white = document.querySelector('.header_logo_white');
-        let menu_name = document.querySelector('.header_menu_burger_name');
-        let menu_burger_dot = document.querySelector('.header_menu_burger_dot');
-        if(window.scrollY >= 200){
-            network_and_sound.style.filter = "invert(0)";  
-        }
-        if(window.scrollY < 200){
-            network_and_sound.style.filter = "invert(1)";  
-        }
-        if(window.scrollY >= 900){
-            header_logo_white.style.opacity = "1";
-            header_logo_back.style.opacity = "0";
-            menu_name.style.color = "black";
-            menu_burger_dot.style.backgroundColor = "black";
-        }
-        if(window.scrollY < 900){
-            header_logo_white.style.opacity = "0";
-            header_logo_back.style.opacity = "1";
-            menu_name.style.color = "white";
-            menu_burger_dot.style.backgroundColor = "white";
-        }
+    
+    let network_and_sound = document.querySelector('.project_page_bottom');
+    let header_logo_back = document.querySelector('.header_logo_back');
+    let header_logo_white = document.querySelector('.header_logo_white');
+    let menu_name = document.querySelector('.header_menu_burger_name');
+    let menu_burger_dot = document.querySelector('.header_menu_burger_dot');
+    if(window.scrollY >= 200){
+        network_and_sound.style.filter = "invert(0)";  
+    }
+    if(window.scrollY < 200){
+        network_and_sound.style.filter = "invert(1)";  
+    }
+    if(window.scrollY >= 900){
+        header_logo_white.style.opacity = "1";
+        header_logo_back.style.opacity = "0";
+        menu_name.style.color = "black";
+        menu_burger_dot.style.backgroundColor = "black";
+    }
+    if(window.scrollY < 900){
+        header_logo_white.style.opacity = "0";
+        header_logo_back.style.opacity = "1";
+        menu_name.style.color = "white";
+        menu_burger_dot.style.backgroundColor = "white";
+    }
 
 
+    // affichage bottom page, network and sound
+    // ---------------------------------------------------------------
 
-        // affichage bottom page, network and sound
-        // ---------------------------------------------------------------
+    // appel de variable dans le script.js
 
-        // appel de variable dans le script.js
+    if(window.scrollY > (document.body.scrollHeight / 1.3)){
+        network_and_sound.style.opacity = 0;
+    }
+    else{
+        network_and_sound.style.opacity = 1;
+    }
 
-        if(window.scrollY > (document.body.scrollHeight / 1.3)){
-            network_and_sound.style.opacity = 0;
-        }
-        else{
-            network_and_sound.style.opacity = 1;
-        }
-
-    }, 10);
-  });
+});
 
 
 // next project 
